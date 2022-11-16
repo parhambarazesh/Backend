05fe1ef4f546ace5a7917d78b398acb846f03582
parhambarazesh <parham.barazesh@gmail.com>
2022-11-16 14:45:21 +0100

    add calculate score for student method

---
+++ QuizClient/Student.cs
@@ -0,0 +1,9 @@
+namespace QuizClient
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int QuizId { get; set; }
+        public int Score { get; set; }
+    }
+}
